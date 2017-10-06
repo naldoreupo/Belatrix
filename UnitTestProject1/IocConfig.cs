8cfdb7f0f88844aa62d9a665d275cd36fae755fe
naldoreupo <naldoreupo@hotmail.com>
2017-10-05 21:48:05 -0500

    Exam

---
+++ UnitTestProject1/IocConfig.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using Autofac;
+using System.Reflection;
+//using Belatrix.Logger.Service.Interfaces;
+using Belatrix.Logger.Domain;
+using Belatrix.Logger.Interfaces;
+using Belatrix.Logger.Transversal;
+using Belatrix.Logger.Service;
+using Belatrix.Logger.Service.Interfaces;
+
+namespace Belatrix.Logger.UnitTest
+{
+    public static class IocConfigTest
+    {
+
+        public static IContainer RegisterIoc()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<JobLoggerFileDomain>().Keyed<IJobloggerDomain>(Configuration.File);
+            builder.RegisterType<JobLoggerDatabaseDomain>().Keyed<IJobloggerDomain>(Configuration.Database);
+
+            builder.RegisterType<JobLoggerService>().As<IJobLoggerService>();
+
+            builder.RegisterAssemblyTypes(Assembly.Load("Belatrix.Logger.Service.Interfaces"))
+           .Where(t => t.Name.EndsWith("Services"))
+           .AsImplementedInterfaces()
+           .InstancePerLifetimeScope();
+
+            builder.RegisterAssemblyTypes(Assembly.Load("Belatrix.Logger.Repository"))
+               .Where(t => t.Name.EndsWith("Repository"))
+               .AsImplementedInterfaces()
+               .InstancePerLifetimeScope();
+
+            return builder.Build();
+        }
+    }
+}
---
+++ UnitTestProject1/JobLoggerTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Belatrix.Logger.Interfaces;
+using Autofac;
+using Belatrix.Logger.Domain.Entities;
+using Belatrix.Logger.Transversal;
+using Presentacion.LoggerService;
+using Belatrix.Logger.Service.DTO;
+
+namespace Belatrix.Logger.UnitTest
+{
+    [TestClass]
+    public class JobLoggerTest
+    {
+        [TestMethod]
+        public void TestSaveDB()
+        {
+            JobLoggerDTO objJobLogger = new JobLoggerDTO();
+            objJobLogger.Message = "Test database " + DateTime.Now.ToLongDateString() + "Message";
+            objJobLogger.Configuration = (int)Configuration.Database;
+            objJobLogger.TypeMessage = (int)TypeMessage.Error;
+            
+            JobLoggerServiceClient srv = new JobLoggerServiceClient();
+            Assert.AreEqual(true, srv.SaveLogger(objJobLogger).Success);
+        }
+        
+        [TestMethod]
+        public void TestSaveFile()
+        {
+            JobLoggerDTO objJobLogger = new JobLoggerDTO();
+            objJobLogger.Message = "Test file " + DateTime.Now.ToLongDateString() + "Message";
+            objJobLogger.Configuration = (int)Configuration.File;
+            objJobLogger.TypeMessage = (int)TypeMessage.Error;
+            
+            JobLoggerServiceClient srv = new JobLoggerServiceClient();
+            Assert.AreEqual(true, srv.SaveLogger(objJobLogger).Success);
+
+        }
+
+    }
+}
