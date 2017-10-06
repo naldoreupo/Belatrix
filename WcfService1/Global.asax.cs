36260124dff01b16b0ff05c92239390d8bd365c5
naldoreupo <naldoreupo@hotmail.com>
2017-10-05 21:33:03 -0500

    Exam
    Examen Beltarix

---
+++ WcfService1/Global.asax.cs
@@ -0,0 +1,53 @@
+﻿using Autofac;
+using Autofac.Integration.Wcf;
+using Belatrix.Logger.Transversal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace Belatrix.Logger.Service
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            Maps.register();
+            IContainer container = IocConfigApp.RegisterIoc();
+            AutofacHostFactory.Container = container;
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WcfService1/IocConfig.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using Autofac;
+using System.Reflection;
+using Belatrix.Logger.Service;
+using Belatrix.Logger.Service.Interfaces;
+using Belatrix.Logger.Interfaces;
+using Belatrix.Logger.Transversal;
+using Belatrix.Logger.Domain;
+
+namespace Belatrix.Logger.Service
+{
+    public static class IocConfigApp
+    {
+
+        public static IContainer RegisterIoc()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<JobLoggerFileDomain>().Keyed<IJobloggerDomain>(Configuration.File);
+            builder.RegisterType<JobLoggerDatabaseDomain>().Keyed<IJobloggerDomain>(Configuration.Database);
+
+            builder.RegisterType<JobLoggerService>().As<IJobLoggerService>();
+
+            builder.RegisterAssemblyTypes(Assembly.Load("Belatrix.Logger.Service.Interfaces"))
+           .Where(t => t.Name.EndsWith("Services"))
+           .AsImplementedInterfaces()
+           .InstancePerLifetimeScope();
+
+            builder.RegisterAssemblyTypes(Assembly.Load("Belatrix.Logger.Repository"))
+               .Where(t => t.Name.EndsWith("Repository"))
+               .AsImplementedInterfaces()
+               .InstancePerLifetimeScope();
+
+            return builder.Build();
+        }
+    }
+}
