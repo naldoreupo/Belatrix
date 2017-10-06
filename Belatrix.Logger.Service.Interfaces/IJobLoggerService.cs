bfa489c546a439c6929320a1d978f91a73c1a499
naldoreupo <naldoreupo@hotmail.com>
2017-10-05 21:35:54 -0500

    Exam
    Exam

---
+++ Belatrix.Logger.Service.Interfaces/IJobLoggerService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Belatrix.Logger.Service.DTO;
+using Belatrix.Logger.Transversal;
+using System.ServiceModel;
+
+namespace Belatrix.Logger.Service.Interfaces
+{
+    [ServiceContract]
+    public interface IJobLoggerService
+    {
+        [OperationContract]
+        Response<Boolean> SaveLogger(JobLoggerDTO logger);
+    }
+}
